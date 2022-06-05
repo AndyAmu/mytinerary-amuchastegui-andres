63e63cc8c614364e976e580d69c9243a0380edc1
Andres Amuchastegui <amuchastegui1994@gmail.com>
2022-06-05 19:06:38 -0300

    Gurdaamos por primera vez MyTinerary

---
+++ src/components/Body/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Body = ()  => {
+    return (
+        <div className='Body-img'>
+            <h1 className='titulo-centro'>My Tinerary</h1>
+        </div>
+    )
+}
+
+export default Body
---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+import { Container, LogoContainer, Menu, MenuItem, MenuItemLink, MobileIcon, Wrapper } from './NavBar-Elements'
+import { FaAvianex, FaAlignJustify, FaUserCircle } from "react-icons/fa";
+import { IconContext } from 'react-icons'; // Estilos globales
+const NavBar = () => {
+    const [showMobileMenu, setMobileMenu] = useState(false)
+    return (
+        <div>
+            <Container>
+                <Wrapper>
+                    <IconContext.Provider value={{ style: {fontSize: "2em" }}}>
+                    <LogoContainer>
+                        <FaAvianex />
+                        <p>My</p>
+                        <p>Tinerary</p>                        
+                    </LogoContainer>
+
+                    <MobileIcon onClick = {() => setMobileMenu(!showMobileMenu)}>
+                        <FaAlignJustify />
+                    </MobileIcon>
+
+                    <Menu open= {showMobileMenu}>
+                        <MenuItem>
+                            <MenuItemLink onClick = {() => setMobileMenu(!showMobileMenu)}>
+                                Home
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink onClick = {() => setMobileMenu(!showMobileMenu)}>
+                                Cities
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <MenuItemLink onClick = {() => setMobileMenu(!showMobileMenu)}>
+                            <div>
+                                <FaUserCircle />
+                            </div>
+                                
+                            </MenuItemLink>
+                        </MenuItem>
+                    </Menu>
+                    </IconContext.Provider>
+                </Wrapper>
+            </Container>
+        </div>
+    )
+}
+
+export default NavBar

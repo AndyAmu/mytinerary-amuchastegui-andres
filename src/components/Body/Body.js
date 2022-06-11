a2c46767c65c7e76ed00ea372fa4d4d2f02a726e
Andres Amuchastegui <amuchastegui1994@gmail.com>
2022-06-11 18:08:44 -0300

    imagenes

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -7,7 +7,8 @@ const Body = () => {
     return (
         <main className='mainPrincipal'>
             <div className='imagenprincipal'>
-                <div className='Body-img'>
+                <div className='Body-img'>+
+                <div className='contenedor-textos-body'>
                     <h1 className='titulo-centro'>My<b className='Tinerary'>Tinerary</b> </h1>
                     <h4 className='texto-body'>"Find your perfect trip, designed by insiders who know and love their cities!"</h4>
                     <div className='boton-llamativo'>
@@ -16,7 +17,7 @@ const Body = () => {
                         <Button onClick={() => { }} className='button-85' variant="warning"><b>Click Here!</b></Button>
                         
                         </LinkRouter>
-                        
+                        </div>
                     </div>
                 </div>
             </div>

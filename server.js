91e69412ce91c32e79fcc80c07ac712d2ffabbb1
Andres Amuchastegui <amuchastegui1994@gmail.com>
2022-06-13 12:43:10 -0300

    Agrego mongo

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+require ('dotenv').config()
+require('./config/database')
+
 const express = require('express')
 const app= express()
+
 
 const PORT = 4000
 app.set('port', PORT)
 
 app.get('/',(req,res) => {
-    res.send('SERVIDOR DE ANDRES AMUCHASTEGUI')
+    res.send('SERVIDOR DE ANDRES ')
 })
 app.listen(PORT, () => {
-    console.log('Servidor crado en puerto: ' + app.get('port'))
+    console.log('Servidor crado en puerto: ' + PORT)
 })

373ff92ab09094106a2b150add941194acc93058
Andres Amuchastegui <amuchastegui1994@gmail.com>
2022-07-12 17:44:44 -0300

    heroku User user

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const passport = require('passport')
 const jwtStrategy = require('passport-jwt').Strategy
 const extractJwt = require('passport-jwt').ExtractJwt
 
-const User = require('../models/User')
+const User = require('../models/user')
 
 module.exports = passport.use(new jwtStrategy({
     jwtFromRequest: extractJwt.fromAuthHeaderAsBearerToken(),
